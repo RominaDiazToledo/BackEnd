8dfc054f16172d5bf24e24aeed78eee32d215835
Romina <romina.diaz.toledo@gmail.com>
2022-11-14 21:29:05 -0300

    Actualización de Experiencia Laboral

---
+++ src/main/java/com/portfolio/nrdt/Dto/DtoExperiencia.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.nrdt.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author romin
+ */
+public class DtoExperiencia {
+    @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+    
+    //Constructores
+
+    public DtoExperiencia() {
+    }
+
+    public DtoExperiencia(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+    //Getters & Setters
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+}
